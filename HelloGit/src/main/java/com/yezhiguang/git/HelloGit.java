5f1bb81f34f25662031b25c690c4f4ccf95e17f4
yezhiguang <3424421082@qq.com>
2022-01-26 17:29:32 +0800

    克隆者添加2

--- HelloGit/src/main/java/com/yezhiguang/git/HelloGit.java
+++ HelloGit/src/main/java/com/yezhiguang/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("在线更新");
         System.out.println("克隆者添加");
+        System.out.println("克隆者添加2");
     }
 }
